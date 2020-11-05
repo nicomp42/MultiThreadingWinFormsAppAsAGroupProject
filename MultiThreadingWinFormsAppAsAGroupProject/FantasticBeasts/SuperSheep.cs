b14f92d18ff65726a907fc966ee802c6a0afdc4a
HennyBicks01 <73999671+HennyBicks01@users.noreply.github.com>
2020-11-05 10:28:48 -0500

    SuperSheep moved and processes added

---
+++ MultiThreadingWinFormsAppAsAGroupProject/FantasticBeasts/SuperSheep.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+using System;
+
+namespace MultiThreadingWinFormsAppAsAGroupProject
+{
+    /// <summary>
+    /// A Fantastic Beast that will help us maintain a secure world
+    /// </summary>
+    class SuperSheep : FantasticBeast
+    {
+        public override void SayHello() {
+            Console.WriteLine("Hello from " + this.GetType());
+        }
+        public override void RunThread()
+        {
+            msg = "Hello from SuperSheep.RunThread()";
+            long num = Convert.ToInt64(request);
+            long largoPrime = -1;
+            while (num % 2 == 0)
+            {
+                largoPrime = 2;
+                num >>= 1;
+            }
+            for (int i = 3; i <= Math.Sqrt(num); i += 2)
+            {
+                while (num % i == 0)
+                {
+                    largoPrime = i;
+                    num = num / i;
+                }
+            }
+            if (num > 2)
+                largoPrime = num;
+
+            response = Convert.ToString(largoPrime);
+        }
+
+    }
+}
